5c83054dd5770aeac1089380465a0c6a791b5a1e
Pranav <pranavpk404@gmail.com>
2022-05-11 13:49:16 +0530

    post page done now comments

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -1,14 +1,64 @@
 import { GetStaticProps } from "next/types";
 import { sanityClient, urlFor } from "../../sanity";
 import { Post } from "../../typings";
-
+import Image from "next/image";
+import PortableText from "react-portable-text";
 interface Props {
   post: Post;
 }
 
 const Post = ({ post }: Props) => {
-  console.log(post);
-  return <div>hello you are at {post.title}</div>;
+  return (
+    <div>
+      <Image
+        width={1200}
+        height={100}
+        className="w-full h-40 object-cover"
+        alt={post.title}
+        src={urlFor(post.mainImage.asset._ref).url()!}
+      />
+      <article className="max-w-3xl mx-auto p-5">
+        <h1 className="text-3xl mb-3 ">{post.title}</h1>
+        <div className="flex items-center mt-2 space-x-5">
+          <Image
+            width="48"
+            height="48"
+            className="rounded-full"
+            src={urlFor(post.author.image).url()!}
+            alt={post.author.name}
+          />
+          <p className="font-extralight text-sm">
+            Blog Post by {post.author.name} - Published at{" "}
+            {new Date(post._createdAt).toLocaleString()}
+          </p>
+        </div>
+        <div>
+          <PortableText
+            className=""
+            serializers={{
+              h1: (props: any) => (
+                <h1 className="text-3xl mb-3">{props.children}</h1>
+              ),
+              h2: (props: any) => (
+                <h2 className="text-2xl mb-3">{props.children}</h2>
+              ),
+              link: (props: any) => (
+                <a className="text-base mb-3" href={props.href}>
+                  {props.children}
+                </a>
+              ),
+              li: ({ props }: any) => (
+                <li className="text-base mb-3">{props.children}</li>
+              ),
+            }}
+            content={post.body}
+            projectId={process.env.SANITY_PROJECT_ID}
+            dataset={process.env.SANITY_DATASET}
+          />
+        </div>
+      </article>
+    </div>
+  );
 };
 
 export default Post;
@@ -54,5 +104,6 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
     props: {
       post,
     },
+    revalidate: 3600,
   };
 };
--- sanity.ts
+++ sanity.ts
@@ -1,8 +1,8 @@
 import { createClient } from "next-sanity";
 import imageUrlBuilder from "@sanity/image-url";
 export const config = {
-  dataset: process.env.NEXT_SANITY_DATASET || "production",
-  projectId: process.env.NEXT_SANITY_PROJECT_ID || "",
+  dataset: process.env.SANITY_DATASET || "production",
+  projectId: process.env.SANITY_PROJECT_ID || "",
   apiVersion: "2021-10-21",
   useCdn: process.env.NODE_ENV === "production",
 };
